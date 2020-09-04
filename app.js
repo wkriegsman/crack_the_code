76121cf89183ccb22f85ab0f7e990191b14173c1
wkriegsman <wkriegsman@gmail.com>
2020-09-04 14:06:55 -0400

    added check code logic

--- app.js
+++ app.js
@@ -2,18 +2,61 @@
 function sayHello(name){
     console.log(`Hello ${name}`)
 }
-
 sayHello("Bill");
 
+const colors = ["purple", "blue", "red", "green", "yellow", "white"]
 
+generateCode = function() {
+    const len =  currentLevel.colors.length,
+        secret = [],
+        i;
 
+    for (i = 0; i < colors.length; i++) {
+      secret[i] = currentLevel.colors[ Math.floor(Math.random() * len) ];
+    }
 
+    return secret;
+  }
+  
+  for (i = 0; i < colors.length; i++){
+      if (pg1 === secret[0]){
+          return pin1
+      }else{(pg1 === secret[i]);{
+          return pin2
+      }
+          
+      }
+  }
 
+  for (i = 0; i < colors.length; i++){
+    if (pg2 === secret[1]){
+        return pin1
+    }else{(pg1 === secret[i]);{
+        return pin2
+    }
+        
+    }
+}
 
+for (i = 0; i < colors.length; i++){
+    if (pg3 === secret[2]){
+        return pin1
+    }else{(pg1 === secret[i]);{
+        return pin2
+    }
+        
+    }
+}
 
-
-
-
+for (i = 0; i < colors.length; i++){
+    if (pg4 === secret[3]){
+        return pin1
+    }else{(pg1 === secret[i]);{
+        return pin2
+    }
+        
+    }
+}
 
 
 
